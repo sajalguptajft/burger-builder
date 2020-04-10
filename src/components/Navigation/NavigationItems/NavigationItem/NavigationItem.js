f05ed0bdaa026169f5bce169ff7d4ba87c870149
Sajal Gupta <sajal.gupta@jellyfishtechnologies.com>
2020-04-10 13:11:53 +0530

    Fix file name

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import classes from './NavigrationItem.css'
+import classes from './NavigationItem.css'
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
