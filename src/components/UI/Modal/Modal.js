42151806f71039d717a6ffdb0eb160c505310e22
Sajal Gupta <sajal.gupta@jellyfishtechnologies.com>
2020-04-12 16:24:46 +0530

    Add error handler

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux/Aux';
+import Aux from '../../../HOC/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
 
     shouldComponentUpdate ( nextProps, nextState ) {
-        return nextProps.show !== this.props.show;
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
     componentWillUpdate () {
